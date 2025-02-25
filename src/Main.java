2618f7ce1090a5c9775b43239d17a7690236dc80
joan-ch-18 <joanchindoyh@gmail.com>
2025-02-25 12:10:18 -0500

    Saludando a Winder

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hola mucho gusto ");
+        System.out.println("Hola gente.");
         System.out.println("Hola Winder");
 
     }
