42e8158879d0b747b19ca0118f8747758fc7998a
joan-ch-18 <joanchindoyh@gmail.com>
2025-02-25 12:07:16 -0500

    Saludo de Prueba

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hola mucho gusto ");
-        System.out.println("Hola Winder");
+        System.out.println("Hola soy Joan.");
+        System.out.println("Hola Winder.");
+
 
     }
 }
